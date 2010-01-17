ebdc6c6e30085b142cc9f10996e92d33246cc05e
Josef Nankivell <josefnankivell@gmail.com>
2010-01-17 19:59:47 +0000

    added closed table to install.sql

--- install/install.sql
+++ install/install.sql
@@ -12,3 +12,29 @@ CREATE TABLE 'bids' (
 PRIMARY KEY ('bid_id')
 KEY 'product_id ('product_id')
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 AUTO_INCREMENT=4;
+
+--
+-- Dumping data for table 'bids'
+--
+
+
+
+----------------------------------------
+
+--
+-- Table structure for table 'closed'
+--
+
+CREATE TABLE 'closed' (
+'product_id' int(11) NOT NULL default '0',
+'winner' varchar(32) NOT NULL default '',
+'bid_amount' float NOT NULL default '0',
+'email_sent' tinyint(1) NOT NULL default '0',
+'admin_email_sent' tinyint(4) NOT NULL default '0',
+'close_date' timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+PRIMARY KEY ('product_id')
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+__
+__ Dumping data for table 'closed'
+__

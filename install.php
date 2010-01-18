9cbca0dfd7642985f9a085b245f51f62f0fc3577
Josef Nankivell <josefnankivell@gmail.com>
2010-01-18 20:45:06 +0000

    added install.php

---
+++ install.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		<h1>Bid4it installation instructions</h1>
+		
+		<h2>System requirments</h2>
+		<ol>
+			<li>PHP 4.3 or higher with short tags on</li>
+			<li>MySQL 4.1 or higher</li>
+		</ol>
+</body>
+</html>

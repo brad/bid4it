cdb53cb8027de9bb9547fa278f1ca6d4752ce157
Josef Nankivell <josefnankivell@gmail.com>
2010-01-31 22:13:23 +0000

    added config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+define('DATAFACE_INSTALLATION_PATH', 'dataface');
+	// Path to dataface install relative to index.php or absolute
+	
+define('DATAFACE_INSTALLATION_URL', 'dataface');
+	// Dataface installation URL relative to index.php or absolute
+?>
